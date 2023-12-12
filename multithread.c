12b0202cf62504b8f590d0eb35b70555b5ca6916
Aywei2001 <aywei2013@gmail.com>
2023-12-12 10:45:43 -0800

    HW  4 Submit

---
+++ multithread.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef struct{
+	int list_start;
+	int list_end;
+	int * list;
+}Sortparameters;
+
+void* sortarrays(void * parameter){
+	Sortparameters *data = (Sortparameters *) malloc(sizeof(parameters));
+}
+
+void* sort_2(void * parameter){
+	Sortparameters *data = (Sortparameters *) malloc(sizeof(parameters));
+}
+
+int main(){
+	printf("Hellow World\n");
+	return 0;
+}
